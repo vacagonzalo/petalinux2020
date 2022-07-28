292c73b9d9c6d6775bee4313a06bd888c7a9a68a
Gonzalo Nahuel Vaca <vacagonzalo@gmail.com>
2022-07-27 21:22:56 -0300

    fix workspace

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
-docker build --build-arg PETA_VERSION=2020.1 --build-arg PETA_RUN_FILE=petalinux-v2020.1-final-installer.run -t petalinux:2020.1 .
+#!/bin/bash
+docker build \
+    --build-arg PETA_VERSION=2020.1 \
+    --build-arg PETA_RUN_FILE=petalinux-v2020.1-final-installer.run \
+    -t petalinux:2020.1 .
--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-docker run -ti -e DISPLAY=$DISPLAY --net="host" --name="petalinux" --hostname="petalinux" -v /home/gonzalo/workspace/:/home/vivado/workspace petalinux:2020.1 /bin/bash
+docker run -ti \
+    -e DISPLAY=$DISPLAY \
+    --net="host" \
+    --name="petalinux" \
+    --hostname="petalinux" \
+    -v /home/gonzalo/workspace/:/home/dev/workspace petalinux:2020.1 \
+    /bin/bash
